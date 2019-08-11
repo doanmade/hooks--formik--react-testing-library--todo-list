8893562aa6b41dbe6ca6186fb4d428b37837ebe5
koennecker <koenneckerb@gmail.com>
2019-08-11 10:54:33 -0700

    Setup

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    task: 'Organize Garage',
+    id: '64dc71e9-798b-440b-bd1e-6613ab9cb5ac',
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: '416096a9-c263-490e-9ebc-b36692849edc',
+    completed: false
+  }
+];
