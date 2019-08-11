193c1cc03ebc881dcd3025304ddf9fde0e61de5e
koennecker <koenneckerb@gmail.com>
2019-08-11 12:44:43 -0700

    Add TodoForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import uuid from 'uuid/v4';
 import './App.css';
 import initialTodos from './data';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = React.useState(initialTodos);
+  const addTodo = task => {
+    const newTodo = {
+      task,
+      id: uuid(),
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const clearCompletedTodos = () =>
+    setTodos(todos.filter(todo => !todo.completed));
+  const toggleComplete = id =>
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+          return todo;
+        } else {
+          return todo;
+        }
+      })
+    );
+
   return (
     <div className='App'>
       <h1>Todo List</h1>
-      <TodoList todos={todos} handleToggleComplete={() => {}} />
+      <TodoList todos={todos} handleToggleComplete={toggleComplete} />
+      <TodoForm addTodo={addTodo} clearTodos={clearCompletedTodos} />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,1 +1,51 @@
 import React from 'react';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+function TodoForm({ clearTodos, errors, isSubmitting }) {
+  return (
+    <div>
+      <Form>
+        <label>
+          Todo
+          <Field name='todo' placeholder='Do the dishes' />
+        </label>
+        <ErrorMessage name='todo'>
+          {err => <div className='error'>{err}</div>}
+        </ErrorMessage>
+        <button
+          type='submit'
+          disabled={isSubmitting || Object.keys(errors).length}>
+          Submit
+        </button>
+      </Form>
+      <button onClick={clearTodos}>Clear</button>
+    </div>
+  );
+}
+
+export default withFormik({
+  mapPropsToValues() {
+    return {
+      todo: ''
+    };
+  },
+  validationSchema: Yup.object().shape({
+    todo: Yup.string()
+      .min(2, 'Must be at least 2 characters')
+      .max(100, "Can't be longer than 100 characters.")
+      .required('Todo is required.')
+  }),
+  handleSubmit(
+    values,
+    {
+      props: { addTodo },
+      resetForm,
+      setSubmitting
+    }
+  ) {
+    addTodo(values.todo);
+    resetForm();
+    setSubmitting(false);
+  }
+})(TodoForm);
